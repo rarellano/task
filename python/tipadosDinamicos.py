9df7739b9def7cd979106b0648762b778d1ae722
Rafael Arellano <rafa.are.ro@gmail.com>
2015-07-03 22:02:34 +0200

    python poquito a poco 0.1
    Signed-off-by: Rafael Arellano <rafa.are.ro@gmail.com>

--- python/tipadosDinamicos.py
+++ python/tipadosDinamicos.py
@@ -17,6 +17,24 @@ lista_2=lista_1
 print (lista_1)
 print (lista_2)
 
-lista_1[0]=0
+lista_1[0]="cabesa"
 print (lista_1)
 print (lista_2)
+
+conjunto = {0,1,2}
+
+conjunto2 = {0,1,21}
+print (conjunto)
+
+print (conjunto & conjunto2)
+
+conjunto.add("cabesa")
+
+print(conjunto)
+
+print('''La cruzcampo de nuestra ciudad,
+	siempre fría tiene que estar,
+	en el baso, 
+	o en la litrona, 
+	siempre fría tiene que estar!!
+	CRUZCAMPO!!''')
